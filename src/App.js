c2310a105a0ae74aa4aca5015fac43f4ff5453c9
ujujk <hgyuij>
2022-06-29 10:27:53 +0900

    create react management

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import TOC from './components/TOC';
+import Subject from './components/Subject';
+import Content from './components/Content';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{  //main 컴포넌트
+  render(){
+    return (
+      <div>
+        <Subject title="WEB" sub="world wide web"></Subject>  {/* <Subject /> */}
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+      </div>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
+    );
+  }
 }
 
 export default App;
