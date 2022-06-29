65ce34a9c798f484b8ecd1c6033225628c38a06f
talkingpotatover2 <gjdlwls95@gmail.com>
2022-06-29 13:37:46 +0900

    modify App.js state

--- src/App.js
+++ src/App.js
@@ -6,11 +6,22 @@ import Subject from './components/Subject';
 import Content from './components/Content';
 
 class App extends Component{  //main 컴포넌트
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB',sub:'World Wide Web'},
+      contents:[
+        {id:1,title:'HTML',desc:'HTML is for information'},
+        {id:2,title:'CSS',desc:'CSS is for design'},
+        {id:3,title:'JavaScript',desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
   render(){
     return (
       <div>
-        <Subject title="WEB" sub="world wide web"></Subject>  {/* <Subject /> */}
-        <TOC></TOC>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>  {/* <Subject /> */}
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
       // <div className="App">
